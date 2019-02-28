b56ccfd1a203ce041e108c38d6fce529edf5a57f
manon jacquin <manon.jacquin@gmail.com>
2019-02-28 15:58:24 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+winner = ""
+    data.each do |seasons, attributes|
+      if seasons == season
+        attributes.each do |people|
+        puts "#{people}"
+          if people["status"] == "Winner"
+            winner = people["name"].split(" ").first
+          end
+        end
+      end
+    end
+   winner
+end 
+
 
 def get_contestant_name(data, occupation)
   # code here
+  winner = ""
+  data.each do |season, attributes|
+    attributes.each do |people|
+      if people["occupation"] == occupation
+      winner = people["name"]
+      end
+    end
+  end
+  winner
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |season, attributes|
+    attributes.each do |people|
+      if people["hometown"] == hometown
+      count += 1
+      end
+    end
+  end
+  count
 end
+
 
 def get_occupation(data, hometown)
   # code here
+  occupation = nil 
+  data.each do |season, attributes|
+    search = attributes.find do |people|
+        people["hometown"] == hometown
+        end
+          if !search
+          else occupation = search["occupation"]
+        end
+    end
+  occupation
 end
+
+
 
 def get_average_age_for_season(data, season)
   # code here
+  
 end
