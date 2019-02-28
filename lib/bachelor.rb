3e4e3493ddb0182d795e3a4101f67310a084e0c1
manon jacquin <manon.jacquin@gmail.com>
2019-02-28 16:18:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -66,7 +66,7 @@ def get_average_age_for_season(data, season)
   persons_number = data[season].length
   data.each do |seasons, attributes|
     if seasons == season
-      persons = attributes.collect do |people|
+      attributes.collect do |people|
         ages += people["age"].to_i
     
       end
